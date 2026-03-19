6f2d1d7f19109ff31929b19158c47ea306cf496d
ralfroemer99 <ralf-roemer@gmx.de>
2026-03-19 18:37:28 +0100

    Increase batch size for real-world training

--- bash/real/pretrain_franka.sh
+++ bash/real/pretrain_franka.sh
@@ -1,17 +1,21 @@
+STEPS=200000;
+LOG_STEPS=100;
+BS=128;
+
 python ./lerobot_lsy/src/lerobot/scripts/train_real.py \
     --seed=42 \
-    --job_name=dit_flow_mt_franka_pretrain \
-    --output_dir=./outputs/real/dit_flow_mt_franka_pretrain \
+    --job_name=dit_flow_mt_franka_pretrain_B$BS \
+    --output_dir=./outputs/real/dit_flow_mt_franka_pretrain_B$BS \
     --dataset.repo_id=continuallearning/pretraining_v2_lerobot30 \
     --dataset.video_backend=pyav \
     --policy.type=ditflow_mt \
     --policy.push_to_hub=false \
-    --batch_size=32 \
+    --batch_size=$BS \
     --num_workers=16 \
-    --steps=200000 \
+    --steps=$STEPS \
     --eval_freq=0 \
     --save_freq=50000 \
-    --log_freq=100 \
+    --log_freq=$LOG_STEPS \
     --wandb.enable=true \
     --wandb.disable_artifact=true \
     --wandb.project=clare_experiments \
